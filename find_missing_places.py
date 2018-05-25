28916bd43cb3bff2abe2945e97b8729f0c857588
alleyr <Rob.Alley@fphcare.co.nz>
2018-05-25 12:33:08 +1200

    Complete implementation

--- find_missing_places.py
+++ find_missing_places.py
@@ -3,6 +3,7 @@ import csv
 import zipfile
 import re
 import sys
+import overpy
 
 class bbox:
    south=90.0
@@ -32,10 +33,15 @@ parser.add_option("-b", "--below", dest="below", type="float",
 parser.add_option("-f", "--listfile",
                   action="store", dest="list_filename", default='file_list.csv',
                   help="Name of CSV file containing the status of the import")
+parser.add_option("-p", "--place", dest="place",
+                  help="Only check those imports that match the place name (regexp supported)")
 parser.add_option("-l", "--left", dest="left", type="float", 
                   help="Specify longitude left of which to find places to import")
 parser.add_option("-r", "--right", dest="right", type="float", 
                   help="Specify longitude right of which to find places to import")
+parser.add_option("-u", "--user",
+                  action="store", dest="user", default='linz_robA',
+                  help="Name of OSM user whose imports are to be checked")
 parser.add_option("-v", "--verbose",
                   action="store_true", dest="verbose", default=False,
                   help="Produce verbose output")
@@ -43,12 +49,11 @@ parser.add_option("-z", "--zipfile",
                   action="store", dest="zip_filename", default='linz_places.zip',
                   help="Name of zip file containing the OSC files to be imported")
 (options, args) = parser.parse_args()
-# TODO: Allow filtering by place
 
 #-----------------------------------------------------------------------------
 # Display start message
 #-----------------------------------------------------------------------------
-print("Searching for locations still to to be imported")
+print("Searching for address imports that don't have a corresponding place in OSM")
 if (options.above != None):
    print("   above (north of) %f" % options.above)
 if (options.below != None):
@@ -57,11 +62,17 @@ if (options.left != None):
    print("   left (west) of %f" % options.left)
 if (options.right != None):
    print("   right (east) of %f" % options.right)
+if options.place != None:
+   print("   filtering by place='%s'" % options.place)
+   place_regex=re.compile(options.place)
 print("")
+sys.stdout.flush()
 
 lat_lon = re.compile('node.*lat="(.*?)".*lon="(.*?)"')
-place_type_re = re.compile('tag.*"addr:(hamlet|suburb)" v=".*?"')
-uploader = "linz_robA"   # TODO: Get this from command line
+# TODO: Do the OSC files only encode addresses as hamlet or suburb?
+place_type_re = re.compile('tag.*"addr:(hamlet|suburb)" v="(.*?)"')
+api = overpy.Overpass()
+not_in_osm = 0
 #-----------------------------------------------------------------------------
 # Read data from address import tracking spreadsheet
 #-----------------------------------------------------------------------------
@@ -75,6 +86,7 @@ with open(options.list_filename, 'rt') as csvfile:
     checked_addresses = 0
     matching_places = 0
     matching_addresses = 0
+    mismatched_types = 0
 
     placesZip = zipfile.ZipFile(options.zip_filename)
 
@@ -92,19 +104,21 @@ with open(options.list_filename, 'rt') as csvfile:
 
         place_imported=False
 
-        if (row['uploader'] == uploader):
+        if (row['uploader'] == options.user):
            checked_places += 1
            place = row['place']
            checked_addresses += addresses
            if options.verbose:
               print("Checking %s" % place)
+              sys.stdout.flush()
 
            osc_file_name = "linz_places/" + place + ".osc"
           
            if ((options.above != None) or 
                (options.below != None) or 
                (options.left != None)  or 
-               (options.right != None)) :
+               (options.right != None) or
+               (options.place != None)) :
 
              try:
                with placesZip.open(osc_file_name) as osc_file:
@@ -116,7 +130,9 @@ with open(options.list_filename, 'rt') as csvfile:
                         ((options.left == None)  or (float(coords.group(2)) < options.left)) and
                         ((options.right == None) or (float(coords.group(2)) > options.right))):
 
-                      place_imported=True
+                      if (options.place == None) or (place_regex.match(place)):
+
+                        place_imported=True
 
                       break
              except:
@@ -128,7 +144,8 @@ with open(options.list_filename, 'rt') as csvfile:
            if place_imported:
              matching_places += 1
              matching_addresses += addresses
-             print ("Place: %s - imported" % (place))
+             if options.verbose:
+               print ("Place: %s - imported" % (place))
              place_bbox = bbox()
              place_type_found = False
              place_type = "UNKNOWN"
@@ -148,16 +165,46 @@ with open(options.list_filename, 'rt') as csvfile:
                 elif (not place_type_found):
                   place_type = place_type_re.search(str(line))
                   if (place_type):
-                    print("   Place: %s Type is %s" % (place, place_type.group(1)))
+                    imported_place_type=place_type.group(1)
+                    imported_place_name=place_type.group(2)
+                    if options.verbose:
+                      print("   Place: %s Type is %s" % (place, place_type.group(1)))
                     place_type_found = True
 
              if (not place_type_found):
+               imported_place_type="UNKNOWN"
+               imported_place_name="UNKNOWN"
                print("*** Place: %s Type is UNKNOWN" % (place))
 
-             # TODO: Query OSM for place in OSM (i.e. node with place=<place_type> and name=<place>)
+             # Query OSM for place in OSM (i.e. node with place=<place_type> and name=<place>)
+             result = api.query("""
+             node(%f,%f,%f,%f) ["place"];
+             out body;
+             """ % (place_bbox.south, place_bbox.west, place_bbox.north, place_bbox.east))
+
+             found_in_osm = False
+             for node in result.nodes:
+               if (node.tags.get("name", "UNKNOWN") == imported_place_name):
+                 found_in_osm = True
+                 if (node.tags.get("place", "UNKNOWN") != imported_place_type):
+                   mismatched_types += 1
+                   print("'%s' uses type '%s' for address nodes but type '%s' on place node" % (imported_place_name, imported_place_type, node.tags.get("place", "UNKNOWN")))
+                 if options.verbose:
+                   print("   OSM place %s" % node.tags.get("place", "UNKNOWN"))
+                   # Encode is required for Windows 
+                   print("   OSM name %s" % node.tags.get("name", "UNKNOWN").encode('ascii', 'ignore'))
+                 break
+
+             if (not found_in_osm):
+               not_in_osm += 1
+               print("%s '%s' does not have a place node in OSM" % (imported_place_type, imported_place_name))
+               sys.stdout.flush()
 
 
 
     print("\nCSV file contains a total of %d places with %d addresses" % (total_places, total_addresses))
-    print("Checked %d places imported by '%s' with %d addresses" % (checked_places, uploader, checked_addresses))
-    print("Found %d places imported by '%s' with %d addresses match restrictions" % (matching_places, uploader, matching_addresses))
+    print("Checked %d places imported by '%s' with %d addresses" % (checked_places, options.user, checked_addresses))
+    print("Found %d places imported by '%s' with %d addresses match restrictions" % (matching_places, options.user, matching_addresses))
+    print("Found %d imported places that don't have a corresponding place node in OSM" % (not_in_osm))
+    print("Found %d imported places that mismatched types" % (mismatched_types))
+
