ba6aeb55fe894841d3912116285563c3d45ac185
alleyr <Rob.Alley@fphcare.co.nz>
2019-04-10 07:42:51 +1200

    Retry on geocoder timeout

--- find_missing_parks.py
+++ find_missing_parks.py
@@ -1,4 +1,10 @@
 #! /bin/python
+# Find Auckland parks that are missing from OSM
+# Input files: AC/ParksAndReserves.csv - List of all Auckland Parks
+#              Auckland_Parks.osm - Extract of OSM data tagged leisure=park within the Auckland region
+
+# Note: Modified to retry on timeout exceptions, needs testing
+#       Re-base on master after pushing latest mods from home
 
 import csv
 import xml.etree.ElementTree as ET
@@ -6,7 +12,14 @@ import xml.etree.ElementTree as ET
 import codecs
 import sys
 import time
-from geopy.geocoders import Nominatim
+from geopy.geocoders import Nominatim # TODO: Consider using Photon instead
+
+from tenacity import retry
+
+# Get co-ordinates for an address, retrying after a 5s pause if there is a timeout
+@retry(wait=wait_fixed(5), retry_if_exception_type(geopy.exc.GeocoderTimeOut))
+def geocode_with_retry(address):
+   return geolocator.geocode(address)
 
 # Enable unicode output to StdOut
 UTF8Writer = codecs.getwriter('utf8')
@@ -25,6 +38,7 @@ osm = ET.Element('osm')
 osm.set('version',"0.6")
 osm.set('generator',"find_missing_parks")
 
+# Build a list of all parks in OSM
 for child in root:
     if child.tag == "way" or child.tag == "relation":
         for tag in child.findall('tag'):
@@ -32,17 +46,18 @@ for child in root:
                 osm_parks.append(tag.attrib['v'])
 
 
+# Search list of Auckland parks for parks not in OSM
 with open("AC/ParksAndReserves.csv", 'rt') as csvfile:
     csvreader = csv.DictReader(csvfile)
 
     count = 1
     for row in csvreader:
-        #print("%s"%row['Description'])
         if row['Description'] in osm_parks:
             print("Found park '%s' in OSM"%row['Description'])
         else:
             print("*** Park '%s' at '%s' not in OSM"%(row['Description'],row['Address']))
-            location = geolocator.geocode(row['Address']+", "+row["Postal Code"])
+            # TODO: Use viewbox=((lat,lng),(lat,lng)) and bounded=True to limit resutls to Auckland?
+            location = geocode_with_retry(row['Address']+", "+row["Postal Code"])
             if location is not None:
                 print("   - At %s,%s"%(location.latitude,location.longitude))
 
